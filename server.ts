58149a28581a45b54c921799aeb7a4cee5f6450d
Fernando Silva <artfernando.carlos@gmail.com>
2025-07-04 17:43:32 -0300

    criando arquivos para o server e atualizando o json

---
+++ server.ts
@@ -0,0 +1,13 @@
+// server.js
+import jsonServer from 'json-server'
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+
+const PORT = process.env.PORT || 4000
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`)
+})
